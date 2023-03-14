8dda286f47d5b28547b141952bc7e6638c71d688
kjenno <507598@vistacollege.nl>
2023-03-14 11:53:20 +0100

    base code

---
+++ wwencription/LoginPagina/loginback.php
@@ -0,0 +1,21 @@
+<?php
+    include('connection.php');
+    if (isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password= $_POST['password'];
+
+        $sql = "select * from login where username = '$username' and password = '$password'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+
+        if($count == 1){
+            header("Location: welcome.php");
+        }
+        else{
+            echo '<script  
+            type="text/javascript">window.onload = function () { alert("Password or Username invalid"); } 
+                </script>'; 
+        }
+    }
+    ?>
